e909fcdf693fc24c47d7f08e7a7898d60d599adf
Pranav Kalra <pranav99kalra@gmail.com>
2019-04-19 12:42:26 -0700

    Toggle color

--- Day7/Toggle Exercise/toggle.js
+++ Day7/Toggle Exercise/toggle.js
@@ -1,5 +1,18 @@
-var button = document.querySelector("button");
+var button = document.getElementById("#button1");
+var isPurple = false;
+
+
+// button.addEventListener("click", function(){
+// 	if(isPurple){
+// 		document.body.style.background = "white";
+// 	} else {
+// 		document.body.style.background = "purple";
+// 	}
+// 	isPurple = !isPurple;
+// });
+
 
 button.addEventListener("click", function () {
-    prompt("pressed");
+    document.body.classList.toggle("purple");
 });
+console.log("working");
