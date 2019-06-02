c2b8e16f8cd2ebe7eb8bdcf3948dcc143d553e8d
Pranav Kalra <pranav99kalra@gmail.com>
2019-06-01 21:12:01 -0700

    routing

---
+++ Day 12/Sending HTML Pages/app.js
@@ -0,0 +1,76 @@
+// var http = require('http');
+
+// var fs = require('fs');
+
+
+// var server = http.createServer(function (req, res) {
+//     console.log("request was made " + req.url);
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+//     myReadStream.pipe(res);
+// });
+
+// server.listen(3000, '127.0.0.1');
+// console.log('Now listening to PORT 3000');
+
+//-----------------------------------------------------------------------------------------
+
+/*
+* Serving JSON to the client
+ */
+
+// var http = require('http');
+
+// var fs = require('fs');
+
+
+// var server = http.createServer(function (req, res) {
+//     console.log("request was made " + req.url);
+//     res.writeHead(200, { 'Content-Type': 'application/json' });
+    
+//     var myObj = {
+//         name: 'Pranav',
+//         job: 'Student',
+//         age: 29
+//     };
+//     //myObj needs to converted into a string
+//     res.end(JSON.stringify(myObj));
+// });
+
+// server.listen(3000, '127.0.0.1');
+// console.log('Now listening to PORT 3000');
+
+//when javascript in front end makes a request teh JSON will be the end will be send
+
+//------------------------------------------------------------------------------------
+
+/*
+* Basic Routing
+ */
+
+var http = require('http');
+
+var fs = require('fs');
+
+
+var server = http.createServer(function (req, res) {
+    console.log("request was made " + req.url);
+    
+    if (req.url === '/home' || req.url === '/') {
+        res.writeHead('200', {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    } else if (req.url === '/contact') {
+        res.writeHead('200', {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/contact.html').pipe(res);
+    } else if(req.url === '/api/ninjas') {
+        var ninjas = [{name: 'pranav', age: 20}, {name: 'jhanvi', age: 18}];
+        res.writeHead('200', {'Content-Type': 'application/JSON'});
+        res.end(JSON.stringify(ninjas));
+    } else {
+        res.writeHead('404', { 'Content-Type': 'text/html' });
+        fs.createReadStream(__dirname + '/error.html').pipe(res);
+    }
+});
+
+server.listen(3000, '127.0.0.1');
+console.log('Now listening to PORT 3000');
